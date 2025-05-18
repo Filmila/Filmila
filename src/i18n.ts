6f8afd4e2d1134b03a9c0ebf4b56d7bb0d171fb9
Filmila <gofilmila@gmail.com>
2025-05-18 14:16:44 -0700

    fix: move translation files to src/locales and update i18n imports

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from '../public/locales/en/translation.json';
-import ar from '../public/locales/ar/translation.json';
+import en from './locales/en/translation.json';
+import ar from './locales/ar/translation.json';
 
 i18n
   .use(LanguageDetector)
